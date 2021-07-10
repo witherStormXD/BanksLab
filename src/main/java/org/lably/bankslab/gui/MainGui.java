fc0fa6735c15d6a3a948cf237423c1d4df7bb6d0
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-07-10 10:45:01 -0500

    Update MainGui.java
    Remove imports and  edit lores

--- src/main/java/org/lably/bankslab/gui/MainGui.java
+++ src/main/java/org/lably/bankslab/gui/MainGui.java
@@ -1,16 +1,12 @@
 package org.lably.bankslab.gui;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.lably.bankslab.BanksLab;
 import org.lably.bankslab.PluginCore;
+import org.lably.bankslab.gui.builders.GUIBuilder;
+import org.lably.bankslab.gui.builders.ItemBuilder;
 import org.lably.bankslab.manager.FileManager;
-import org.lably.bankslab.utils.TextColor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainGui {
     private final PluginCore core;
@@ -18,22 +14,30 @@ public class MainGui {
     public MainGui(PluginCore core){
         this.core = core;
     }
-    public void createGui(Player player){
+
+    public void createGui(Player player) {
         GUIBuilder guiBuilder = new GUIBuilder();
+
         FileManager gui = core.getFilesLoader().getGui();
+
         Inventory inventory = guiBuilder.create(player,36,gui.getString("Title-Gui"));
-        List<String> depositlore2 = gui.getStringList("Lore-Deposit-Item");
-        for (String depositLore:gui.getStringList("Lore-Deposit-Item")){
-            depositlore2.add(TextColor.colorized(depositLore));
-            ItemStack depositItem = new ItemBuilder().create("IRON_INGOT",gui.getString("Deposit-Item-Name"),1,depositlore2);
-            inventory.setItem(gui.getInt("Deposit-slot"),depositItem);
-        }
-        List<String> leaveMoneylore = gui.getStringList("LeaveMoney-Item-lore");
-        for(String leaveMoneyFor: gui.getStringList("LeaveMoney-Item-lore")){
-            leaveMoneylore.add(TextColor.colorized(leaveMoneyFor));
-            ItemStack leaveMoney = new ItemBuilder().create("REDSTONE",gui.getString("LeaveMoney-Item-Name"),1,leaveMoneylore);
-            inventory.setItem(gui.getInt("LeaveMoney-slot"),leaveMoney);
-        }
+
+        ItemStack depositItem = new ItemBuilder().create("IRON_INGOT",
+                gui.getString("Deposit-Item-Name"),
+                1,
+                gui.getStringList("Lore-Deposit-Item"));
+
+
+        inventory.setItem(gui.getInt("Deposit-slot"), depositItem);
+
+        ItemStack leaveMoney = new ItemBuilder().create(
+                "REDSTONE",
+                gui.getString("LeaveMoney-Item-Name"),
+                1,
+                gui.getStringList("LeaveMoney-Item-lore"));
+
+        inventory.setItem(gui.getInt("LeaveMoney-slot"), leaveMoney);
+
         player.openInventory(inventory);
 
 

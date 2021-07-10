c3a5a3d8c1a05652591ea4b98b6c175c0073e651
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-07-10 10:45:32 -0500

    Create package "builder"

---
+++ src/main/java/org/lably/bankslab/gui/builders/ItemBuilder.java
@@ -0,0 +1,26 @@
+package org.lably.bankslab.gui.builders;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.lably.bankslab.utils.TextColor;
+
+import java.util.List;
+
+public class ItemBuilder {
+
+    public ItemStack create(String material, String displayName, int amount, List<String> lore) {
+
+        ItemStack itemStack = new ItemStack(Material.valueOf(material), amount);
+
+        ItemMeta itemMeta = itemStack.getItemMeta();
+
+        itemMeta.setDisplayName(TextColor.colorized(displayName));
+
+        itemMeta.setLore(lore);
+
+        itemStack.setItemMeta(itemMeta);
+
+        return itemStack;
+    }
+}

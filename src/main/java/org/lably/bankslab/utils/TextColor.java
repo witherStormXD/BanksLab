9a209b6c983184935a6fbb1070d3984632df80db
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-07-10 10:44:36 -0500

    Update TextColor.java
    Add static metod of String List

--- src/main/java/org/lably/bankslab/utils/TextColor.java
+++ src/main/java/org/lably/bankslab/utils/TextColor.java
@@ -2,10 +2,19 @@ package org.lably.bankslab.utils;
 
 import org.bukkit.ChatColor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TextColor {
 
     public static String colorized(String path){
         return ChatColor.translateAlternateColorCodes('&', path);
+    }
+
+    public static List<String> listColorized(List<String> path){
+        List<String> list = new ArrayList<>(path);
+        list.replaceAll(line -> ChatColor.translateAlternateColorCodes('&', line));
+        return list;
     }
 
 

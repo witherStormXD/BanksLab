ed174aa403bf9c1f3b5cc3c6fd1f048cb613c6b4
Jonathan Narvaez <32984331+Jonakls@users.noreply.github.com>
2021-07-27 21:01:58 -0500

    Added general listener

--- src/main/java/org/lably/bankslab/listener/InventoryClickListener.java
+++ src/main/java/org/lably/bankslab/listener/InventoryClickListener.java
@@ -1,45 +1,39 @@
 package org.lably.bankslab.listener;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.ItemStack;
 import org.lably.bankslab.PluginCore;
 import org.lably.bankslab.manager.FileManager;
 
-import java.util.Set;
-
 public class InventoryClickListener implements Listener {
 
-    private final PluginCore pluginCore;
+    private final PluginCore core;
 
-    public InventoryClickListener(PluginCore pluginCore) {
-        this.pluginCore = pluginCore;
+    public InventoryClickListener(PluginCore core) {
+        this.core = core;
     }
 
     @EventHandler
-    public void onInventoryClick(InventoryClickEvent event) {
-        FileManager gui = pluginCore.getFilesLoader().getGui();
+    public void onClickGeneralGui(InventoryClickEvent event) {
+        FileManager gui = core.getFilesLoader().getGui();
 
         if (!event.getCurrentItem().hasItemMeta()) return;
+        if(!event.getInventory().getTitle().equals(gui.getString("General.Title-Gui"))) return;
+        event.setCancelled(true);
 
+        String itemName = event.getCurrentItem().getItemMeta().getDisplayName();
 
-        for (String path : gui.getConfigurationSection("Icons").getKeys(false)) {
+        if(itemName.equals(gui.getString("General.Icons.Deposit.Item-Name"))) {
 
-            if(event.getCurrentItem().getItemMeta().getDisplayName()
-                    .equals(gui.getString("Icons." + path + ".Item-Name"))) {
-
-                /*
-
-                Pending to propose the movements or actions when clicking on an item in the main menu.
-
-                */
-
-            }
-
+            core.getMainGui().depositGui((Player) event.getWhoClicked());
+            return;
         }
 
+        if(itemName.equals(gui.getString("General.Icons.Leave.Item-Name"))) {
+
+            event.getWhoClicked().closeInventory();
+        }
     }
-
-
 }
